dc2d6feb3a74585782241a55e2fe1202f3eae9d6
Egemen Oner <egemen.oner@broadangle.com>
2022-12-14 18:22:09 +0300

    Development

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const SplashScreen(title: ''),
+    return const MaterialApp(
+      home: SplashScreen(title: ''),
     );
   }
 }
@@ -40,9 +37,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
